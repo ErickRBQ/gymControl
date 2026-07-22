f761e082d8c0dd7c855ded4c2e6fa042742abacf
erickRBQ <erickjrogelbaque@gmail.com>
2026-07-21 19:20:05 -0500

    Delete java directory

--- java/unl/edu/ec/jakarta/hello/EstadoMembresia.java
+++
@@ -1,5 +0,0 @@
-package unl.edu.ec.jakarta.hello;
-
-public enum EstadoMembresia {
-    ACTIVA, VENCIDA, CANCELADA, PENDIENTE
-}
--- java/unl/edu/ec/jakarta/hello/Pago.java
+++
@@ -1,6 +0,0 @@
-package unl.edu.ec.jakarta.hello;
-
-public interface Pago {
-    boolean procesarPago(double monto);
-    String obtenerComprobante();
-}
